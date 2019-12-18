518729e2d6d2001d731be29a3b56832f0d9112a6
gaoly <shuiruohanyu@foxmail.com>
2019-12-18 16:25:08 +0800

    ## 黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home'
+import Login from '../views/login'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/home' // 强制跳转
+  },
+  {
+    path: '/home',
     name: 'home',
     component: Home
+  }, {
+    path: '/login',
+    component: Login
   }
   // {
   //   path: '/about',
---
+++ src/views/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>主页</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

906d720eca0a7f45a1244839d457580db1c0eaf8
gaoly <shuiruohanyu@foxmail.com>
2019-12-18 16:03:15 +0800

    黑马头条PC-初始化项目-安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' // 引入模块
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
 Vue.use(VueRouter)
 
 const routes = [

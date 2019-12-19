3363de6846c296570227cab1488ff963bc0aca77
gaoly <shuiruohanyu@foxmail.com>
2019-12-19 16:19:15 +0800

    黑马头条PC-主页模块-左侧导航菜单

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class='layout-aside'>
+      <div class='title'>
+          <img src="../../assets/img/logo_admin.png" alt="">
+      </div>
+      <!-- 左侧导航组件 -->
+      <el-menu background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
+          <!-- 没有折叠选项 -->
+          <el-menu-item>首页</el-menu-item>
+          <!-- 二级折叠菜单 -->
+          <el-submenu>
+              <!-- 具名插槽 -->
+              <span slot='title'>内容管理</span>
+              <!-- 匿名插槽 -->
+             <el-menu-item>发布文章</el-menu-item>
+             <el-menu-item>文章列表</el-menu-item>
+             <el-menu-item>评论管理</el-menu-item>
+             <el-menu-item>素材管理</el-menu-item>
+
+          </el-submenu>
+          <el-submenu>
+              <!-- 具名插槽 -->
+              <span slot='title'>粉丝管理</span>
+              <!-- 匿名插槽 -->
+             <el-menu-item>图文数据</el-menu-item>
+             <el-menu-item>粉丝概况</el-menu-item>
+             <el-menu-item>粉丝画像</el-menu-item>
+             <el-menu-item>粉丝列表</el-menu-item>
+          </el-submenu>
+          <el-menu-item>账户信息</el-menu-item>
+
+      </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+ .layout-aside {
+     width: 230px;
+     .el-menu {
+       border-right:none;
+     };
+     .title {
+         background-color: #2e2f32;
+         text-align: center;
+         padding: 10px 0;
+         img {
+             height: 35px;
+         }
+     }
+ }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,8 +2,8 @@
   <!-- 放置一个大容器 -->
   <el-container>
     <!-- 左右布局 -->
-    <el-aside>
-      左侧内容
+    <el-aside style="height:100vh;background-color:#353b4e;width:230px;">
+      <layout-aside></layout-aside>
     </el-aside>
     <!-- 右侧容器 -->
     <el-container>
@@ -17,8 +17,11 @@
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 

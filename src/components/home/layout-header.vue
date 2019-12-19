3f8b844ebd68a2a07cd751183de48aabc28f4bff
gaoly <shuiruohanyu@foxmail.com>
2019-12-19 17:55:08 +0800

    黑马头条PC-主页模块-头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -10,13 +10,13 @@
          <el-row type='flex' justify="end" align="middle">
              <img :src="userInfo.photo ? userInfo.photo : defaultImg" alt="">
              <!-- 下拉菜单 -->
-             <el-dropdown>
+             <el-dropdown @command="clickMenu">
                  <!-- 匿名插槽  下拉菜单显示的元素内容 -->
                  <span>{{ userInfo.name }}</span>
                  <el-dropdown-menu slot="dropdown">
-                     <el-dropdown-item>个人信息</el-dropdown-item>
-                     <el-dropdown-item>git地址</el-dropdown-item>
-                     <el-dropdown-item>退出</el-dropdown-item>
+                     <el-dropdown-item command="info">个人信息</el-dropdown-item>
+                     <el-dropdown-item command="git">git地址</el-dropdown-item>
+                     <el-dropdown-item command="lgout">退出</el-dropdown-item>
                  </el-dropdown-menu>
              </el-dropdown>
          </el-row>
@@ -44,6 +44,21 @@ export default {
     }).then(result => {
       this.userInfo = result.data.data
     })
+  },
+  methods: {
+    //   点击菜单项时触发
+    clickMenu (command) {
+      if (command === 'info') {
+
+      } else if (command === 'git') {
+        //   跳转到git地址
+        window.location.href = 'https://github.com/shuiruohanyu/90heimatoutiao'
+      } else {
+        //    退出
+        window.localStorage.removeItem('user-token') // 删除令牌
+        this.$router.push('/login') // 回到登录页
+      }
+    }
   }
 }
 </script>

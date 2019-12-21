5e4d3e0b313f3674747af41e4cf9fab3bd064ce5
gaoly <shuiruohanyu@foxmail.com>
2019-12-21 18:21:51 +0800

    黑马头条PC-评论列表-打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -20,7 +20,7 @@
              <!-- 作用域插槽 -->
             <el-button size="small" type="text">修改</el-button>
             <!-- 需要根据状态来进行判断是关闭还是打开 -->
-            <el-button  size="small" type="text"> {{ obj.row.comment_status ? '关闭' : '打开' }}评论
+            <el-button @click="openOrCloseState(obj.row)" size="small" type="text"> {{ obj.row.comment_status ? '关闭' : '打开' }}评论
 </el-button>
             </template>
 
@@ -53,6 +53,23 @@ export default {
       // celllValue => 当前的单元格的值
       // index 索引
       return cellValue ? '正常' : '关闭'
+    },
+    // 打开或者关闭评论
+    openOrCloseState (row) {
+      // 直接调用接口
+      let mess = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`您是否确定要${mess}评论吗`, '提示').then(() => {
+        // 调用接口
+        this.$axios({
+          method: 'put',
+          url: '/comments/status',
+          params: { article_id: row.id },
+          data: { allow_comment: !row.comment_status } // 因为当前如果是打开 ,就要关闭 如果是关闭 就要打开
+        }).then(result => {
+          //  表示执行成功
+          this.getComment() // 重新拉取评论管理数据
+        })
+      })
     }
   },
   created () {

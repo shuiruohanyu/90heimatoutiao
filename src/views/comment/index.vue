47aa56c379364ba4ff7e33030a044a2848005542
gaoly <shuiruohanyu@foxmail.com>
2019-12-21 17:20:06 +0800

    黑马头条PC-评论列表-页面结构-table

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-      评论列表
-  </div>
+  <!-- 卡片组件 -->
+  <el-card>
+      <bread-crumb slot="header">
+        <!-- 插槽内容 -->
+         <template slot="title">
+             评论管理
+         </template>
+      </bread-crumb>
+      <!-- el-table -->
+      <!-- 表格 -->
+      <el-table>
+          <!-- 放置列组件 -->
+          <el-table-column width="600" label="标题"></el-table-column>
+          <el-table-column label="评论状态"></el-table-column>
+          <el-table-column label="总评论数"></el-table-column>
+          <el-table-column label="粉丝评论数"></el-table-column>
+          <el-table-column label="操作"></el-table-column>
+
+      </el-table>
+  </el-card>
 </template>
 
 <script>

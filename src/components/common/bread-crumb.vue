0860d67f472670797da70db473f831b82adf85e0
gaoly <shuiruohanyu@foxmail.com>
2019-12-21 16:59:29 +0800

    黑马头条PC-面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- 封装原来的el-breadcrumb -->
+<el-breadcrumb separator=">">
+    <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>
+        <!-- 定义一个具名插槽 -->
+        <slot name='title'></slot>
+    </el-breadcrumb-item>
+ </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
-
+import breadCrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside) // 注册一个全局组件
     Vue.component('layout-header', layoutHeader) // 注册一个全局组件
+    Vue.component('bread-crumb', breadCrumb) // 全局注册一个面包屑组件
   }
 }

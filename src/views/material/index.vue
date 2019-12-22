0b828c386d61b27c17e0e781ea00d1c671ae068d
gaoly <shuiruohanyu@foxmail.com>
2019-12-22 17:29:04 +0800

    黑马头条PC-素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -21,6 +21,7 @@
                  </el-row>
                </el-card>
             </div>
+
           </el-tab-pane>
           <el-tab-pane label="收藏图片" name="collect">
               <div class='img-list'>
@@ -31,6 +32,18 @@
             </div>
           </el-tab-pane>
       </el-tabs>
+      <!-- 公共分页组件 -->
+           <el-row type="flex" justify="center">
+              <el-pagination
+                  :current-page="page.currentPage"
+                  :page-size="page.pageSize"
+                  :total="page.total"
+                  @current-change="changePage"
+                  background
+                  layout="prev, pager, next"
+                 >
+              </el-pagination>
+            </el-row>
   </el-card>
 </template>
 
@@ -39,12 +52,23 @@ export default {
   data () {
     return {
       activeName: 'all', // 当前选中的标签
-      list: [] // 接收素材数据
+      list: [], // 接收素材数据
+      page: {
+        currentPage: 1,
+        pageSize: 8,
+        total: 0
+      }
     }
   },
   methods: {
+    // 改变页码方法
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()
+    },
     // 切换页签方法
     changeTab () {
+      this.page.currentPage = 1 // 重置回第一页
       this.getMaterial() // 调用获取数据方法
     },
     // 获取素材的方法
@@ -52,10 +76,13 @@ export default {
       this.$axios({
         url: '/user/images',
         params: {
+          page: this.page.currentPage,
+          per_page: this.page.pageSize,
           collect: this.activeName === 'collect' // 传false是获取所有的数据 传true是获取收藏数据
         }
       }).then(result => {
         this.list = result.data.results // 获取图片数据 有可能是 全部 也由可能是收藏
+        this.page.total = result.data.total_count // 总条数
       })
     }
   },

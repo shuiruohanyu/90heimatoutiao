c755a4cb2d8e0a7ef06400880b615ffcc0641f20
gaoly <shuiruohanyu@foxmail.com>
2019-12-22 18:00:09 +0800

    黑马头条PC-素材列表-素材的上传

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,49 +1,52 @@
 <template>
-  <el-card>
-      <!-- 头部内容 -->
-      <bread-crumb slot="header">
-        <template slot="title">
-           素材管理
-        </template>
-      </bread-crumb>
-      <!-- 标签页 -->
-      <el-tabs v-model="activeName" @tab-click="changeTab">
-          <!-- 标签 -->
-          <el-tab-pane label="全部图片" name="all">
-            <!-- 生成页面结构 -->
-            <div class='img-list'>
-               <!-- v-for对数据进行遍历 -->
-               <el-card class='img-card' v-for="item in list" :key="item.id">
-                 <img :src="item.url" alt="">
-                 <el-row class='operate' type="flex" align="middle" justify="space-around">
-                   <i class='el-icon-star-on'></i>
-                   <i class='el-icon-delete-solid'></i>
-                 </el-row>
-               </el-card>
-            </div>
+  <el-card v-loading="loading">
+    <!-- 头部内容 -->
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <!-- 上传 -->
+    <el-row type='flex' justify="end">
+      <el-upload action="" :http-request="uploadImg" :show-file-list="false">
+        <el-button size="small" type="primary">上传图片</el-button>
+      </el-upload>
+    </el-row>
 
-          </el-tab-pane>
-          <el-tab-pane label="收藏图片" name="collect">
-              <div class='img-list'>
-               <!-- v-for对数据进行遍历 -->
-               <el-card class='img-card' v-for="item in list" :key="item.id">
-                 <img :src="item.url" alt="">
-               </el-card>
-            </div>
-          </el-tab-pane>
-      </el-tabs>
-      <!-- 公共分页组件 -->
-           <el-row type="flex" justify="center">
-              <el-pagination
-                  :current-page="page.currentPage"
-                  :page-size="page.pageSize"
-                  :total="page.total"
-                  @current-change="changePage"
-                  background
-                  layout="prev, pager, next"
-                 >
-              </el-pagination>
+    <!-- 标签页 -->
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+      <!-- 标签 -->
+      <el-tab-pane label="全部图片" name="all">
+        <!-- 生成页面结构 -->
+        <div class="img-list">
+          <!-- v-for对数据进行遍历 -->
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+            <el-row class="operate" type="flex" align="middle" justify="space-around">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
             </el-row>
+          </el-card>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="收藏图片" name="collect">
+        <div class="img-list">
+          <!-- v-for对数据进行遍历 -->
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
+    <!-- 公共分页组件 -->
+    <el-row type="flex" justify="center">
+      <el-pagination
+        :current-page="page.currentPage"
+        :page-size="page.pageSize"
+        :total="page.total"
+        @current-change="changePage"
+        background
+        layout="prev, pager, next"
+      ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -51,6 +54,7 @@
 export default {
   data () {
     return {
+      loading: false,
       activeName: 'all', // 当前选中的标签
       list: [], // 接收素材数据
       page: {
@@ -61,6 +65,20 @@ export default {
     }
   },
   methods: {
+    // 上传图片方法
+    uploadImg (params) {
+      this.loading = true // 先弹个层
+      let data = new FormData()
+      data.append('image', params.file) // 文件加入到参数中
+      this.$axios({
+        method: 'post',
+        url: '/user/images',
+        data
+      }).then(result => {
+        this.loading = false // 关闭弹层
+        this.getMaterial() // 直接调用拉取数据的方法
+      })
+    },
     // 改变页码方法
     changePage (newPage) {
       this.page.currentPage = newPage
@@ -93,27 +111,27 @@ export default {
 </script>
 
 <style lang='less' scoped>
- .img-list {
-   display: flex;
-   flex-wrap: wrap;
-   .img-card {
-     width:200px;
-     height: 220px;
-     margin: 20px 50px;
-     position: relative;
-     img {
-       width: 100%;
-       height: 100%;
-     }
-     .operate {
-       width:100%;
-       position: absolute;
-       left:0;
-       bottom:0;
-       font-size: 20px;
-       height: 36px;
-       background-color: #f4f5f6;
-     }
-   }
- }
+.img-list {
+  display: flex;
+  flex-wrap: wrap;
+  .img-card {
+    width: 200px;
+    height: 220px;
+    margin: 20px 50px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    .operate {
+      width: 100%;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      font-size: 20px;
+      height: 36px;
+      background-color: #f4f5f6;
+    }
+  }
+}
 </style>

d4ea3113dc9cda86feab42bd6fea8c54a0aefc20
gaoly <shuiruohanyu@foxmail.com>
2019-12-22 16:24:05 +0800

    黑马头条PC-素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -6,12 +6,22 @@
            素材管理
         </template>
       </bread-crumb>
+      <!-- 标签页 -->
+      <el-tabs v-model="activeName">
+          <!-- 标签 -->
+          <el-tab-pane label="全部图片" name="all">全部</el-tab-pane>
+          <el-tab-pane label="收藏图片" name="collect">收藏</el-tab-pane>
+      </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all' // 当前选中的标签
+    }
+  }
 }
 </script>
 

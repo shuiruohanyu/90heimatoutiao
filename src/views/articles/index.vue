de7de14f0956b473e0100c5ce670812f4858e724
gaoly <shuiruohanyu@foxmail.com>
2019-12-24 17:21:30 +0800

    黑马头条PC-内容列表-页面结构-搜索筛选

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -7,7 +7,15 @@
       <el-form style="padding-left:50px">
           <el-form-item label="文章状态:">
               <!-- 放置一个单选组  文章状态，0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部-->
-              <el-radio-group v-model="searchForm.status">
+              <!-- 第一种 用监听组件的形式去做搜索 -->
+                 <!-- <el-radio-group v-model="searchForm.status" @change="changeCondition">
+                  <el-radio :label="5">全部</el-radio>
+                  <el-radio :label="0">草稿</el-radio>
+                  <el-radio :label="1">待审核</el-radio>
+                  <el-radio :label="2">审核通过</el-radio>
+                  <el-radio :label="3">审核失败</el-radio>
+              </el-radio-group> -->
+              <el-radio-group v-model="searchForm.status" >
                   <!-- label -->
                   <el-radio :label="5">全部</el-radio>
                   <el-radio :label="0">草稿</el-radio>
@@ -17,6 +25,10 @@
               </el-radio-group>
           </el-form-item>
           <el-form-item label="频道列表:">
+              <!-- 第一种 用监听组件的形式去做搜索 -->
+               <!-- <el-select @change="changeCondition" placeholder="请选择频道" v-model="searchForm.channel_id">
+                  <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+              </el-select> -->
               <el-select placeholder="请选择频道" v-model="searchForm.channel_id">
                   <!-- el-option label是显示值 value是存储值 -->
                   <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
@@ -24,7 +36,9 @@
           </el-form-item>
           <el-form-item label="时间选择:">
               <!-- 日期选择器 日期范围-->
-              <el-date-picker v-model="searchForm.dateRange" type="daterange"></el-date-picker>
+              <!-- 第一种 用监听组件的形式去做搜索 -->
+            <!-- <el-date-picker @change="changeCondition" value-format="yyyy-MM-dd" v-model="searchForm.dateRange" type="daterange"></el-date-picker> -->
+              <el-date-picker  value-format="yyyy-MM-dd" v-model="searchForm.dateRange" type="daterange"></el-date-picker>
           </el-form-item>
       </el-form>
       <el-row class='total' type='flex' align="middle">
@@ -66,6 +80,16 @@ export default {
       defaultImg: require('../../assets/img/default.jpg') // 默认图片
     }
   },
+  watch: {
+    searchForm: {
+      handler: function () {
+        // 此时数据已经变成最新的了
+        // this 指向组件实例
+        this.changeCondition() // 直接调用条件改变的方法
+      },
+      deep: true
+    }
+  },
   filters: {
     //   过滤器第一个参数 是value
     filterStatus (value) {
@@ -100,6 +124,16 @@ export default {
     }
   },
   methods: {
+    //   改变条件
+    changeCondition () {
+      let params = {
+        status: this.searchForm.status === 5 ? null : this.searchForm.status, // 因为5是前端定义的一个标识, 如果等于5 表示查全部, 全部应该什么都不传 直接传null
+        channel_id: this.searchForm.channel_id,
+        begin_pubdate: this.searchForm.dateRange.length ? this.searchForm.dateRange[0] : null, // 开始时间
+        end_pubdate: this.searchForm.dateRange.length > 1 ? this.searchForm.dateRange[1] : null // 截止时间
+      }
+      this.getArticles(params)
+    },
     //   获取所有的频道
     getChannels () {
       this.$axios({
@@ -109,9 +143,10 @@ export default {
       })
     },
     // 获取文章列表数据
-    getArticles () {
+    getArticles (params) {
       this.$axios({
-        url: '/articles'
+        url: '/articles',
+        params
       }).then(result => {
         this.list = result.data.results // 获取文章列表数据
       })

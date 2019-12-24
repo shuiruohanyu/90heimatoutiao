bc445a2d955d0b807ce283349dfeb91b7dbe0ec3
gaoly <shuiruohanyu@foxmail.com>
2019-12-24 18:17:43 +0800

    黑马头条PC-发表文章-页面结构-简单实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -5,6 +5,33 @@
             发布文章
         </template>
       </bread-crumb>
+      <!-- 容器 -->
+      <el-form style="margin-left:50px" label-width="100px">
+          <el-form-item label="标题">
+              <el-input style="width:60%"></el-input>
+          </el-form-item>
+          <el-form-item label="内容">
+                 <el-input
+                 type="textarea"
+                :rows="4"></el-input>
+          </el-form-item>
+          <el-form-item label="封面">
+              <el-radio-group>
+                  <el-radio>单图</el-radio>
+                  <el-radio>三图</el-radio>
+                  <el-radio>无图</el-radio>
+                  <el-radio>自动</el-radio>
+              </el-radio-group>
+          </el-form-item>
+          <el-form-item label="频道">
+              <el-select></el-select>
+          </el-form-item>
+          <el-form-item>
+              <el-button type='primary'>发布</el-button>
+              <el-button>存入草稿</el-button>
+
+          </el-form-item>
+      </el-form>
   </el-card>
 </template>
 

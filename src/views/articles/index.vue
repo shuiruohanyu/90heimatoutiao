60eb0ac6fc0df2701eb3b3c5055af1089f03d4e3
gaoly <shuiruohanyu@foxmail.com>
2019-12-24 16:08:17 +0800

    黑马头条PC-内容列表-页面主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card class='articles'>
       <bread-crumb slot='header'>
          <template slot='title'>文章列表</template>
       </bread-crumb>
@@ -27,6 +27,27 @@
               <el-date-picker v-model="searchForm.dateRange" type="daterange"></el-date-picker>
           </el-form-item>
       </el-form>
+      <el-row class='total' type='flex' align="middle">
+          <span>
+              共找到10000条符合条件的内容
+          </span>
+      </el-row>
+      <div class='article-item' v-for="item in 100" :key="item">
+          <!-- 左侧 -->
+          <div class='left'>
+              <img src="../../assets/img/default.jpg" alt="">
+              <div class='info'>
+                  <span>90期的弟弟们，哈哈哈啊哈哈哈嗝</span>
+                  <el-tag class='tag'>标签一</el-tag>
+                  <span class='date'>2019-12-24 15:07:01</span>
+              </div>
+          </div>
+          <!-- 右侧 -->
+          <div class='right'>
+              <span><i class="el-icon-edit"></i>修改</span>
+              <span><i class="el-icon-delete"></i>删除</span>
+          </div>
+      </div>
   </el-card>
 </template>
 
@@ -58,6 +79,47 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+  .articles {
+      .total {
+          height: 60px;
+          border-bottom: 1px dashed #ccc;
+      }
+      .article-item {
+          display: flex;
+          justify-content: space-between;
+          padding: 20px 0;
+          border-bottom: 1px solid #f2f3f5;
+          .left {
+              display: flex;
+              img {
+                  width: 180px;
+                  height: 100px;
+                  border-radius: 4px;
+              }
+              .info {
+                  height: 100px;
+                  margin-left: 10px;
+                  display: flex;
+                  flex-direction: column;
+                  justify-content: space-around;
+                  .date {
+                      color: #999;
+                      font-size:12px;
+                  }
+                  .tag {
+                      text-align: center;
+                      width:60px;
+                  }
+              }
+          }
+          .right {
+              span {
+                  font-size:14px;
+                  margin-right: 8px;
+                  cursor: pointer;
+              }
+          }
+      }
+  }
 </style>

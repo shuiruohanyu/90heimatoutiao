4f8231c938c04b6ff43e109caf85f8066277b1d0
gaoly <shuiruohanyu@foxmail.com>
2019-12-24 14:46:38 +0800

    黑马头条PC-内容列表-新建页面-挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,9 @@ const routes = [
     }, {
       path: 'material',
       component: () => import('../views/material')
+    }, {
+      path: 'articles', // 文章列表
+      component: () => import('../views/articles')
     }]
   }, {
     path: '/login',
---
+++ src/views/articles/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-card>
+      <bread-crumb slot='header'>
+         <template slot='title'>文章列表</template>
+      </bread-crumb>
+  </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

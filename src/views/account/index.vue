684450ddd5b4d708f752483c27df1a8a6a052e58
gaoly <shuiruohanyu@foxmail.com>
2019-12-27 16:24:09 +0800

    黑马头条PC-账户信息-页面结构

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,6 +3,28 @@
       <bread-crumb slot="header">
         <template slot='title'>账户信息</template>
       </bread-crumb>
+      <!-- 表单 => 表单容器 -->
+      <el-form style="margin-left:100px" label-width="100px">
+          <el-form-item label="用户名">
+              <el-input style="width:40%"></el-input>
+          </el-form-item>
+           <el-form-item label="简介">
+               <el-input style="width:40%"></el-input>
+           </el-form-item>
+           <el-form-item label="邮箱">
+               <el-input style="width:40%"></el-input>
+           </el-form-item>
+           <el-form-item label="手机号">
+               <el-input disabled style="width:40%"></el-input>
+           </el-form-item>
+           <el-form-item>
+               <el-button type="primary">保存信息</el-button>
+           </el-form-item>
+      </el-form>
+      <!-- 上传组件 -->
+      <el-upload class='head-upload' action="" :show-file-list="false">
+          <img src="../../assets/img/cat.jpg" alt="">
+      </el-upload>
   </el-card>
 </template>
 
@@ -12,6 +34,15 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+   .head-upload {
+      position: absolute;
+      right: 300px;
+      top:200px;
+       img {
+          width: 200px;
+          height: 200px;
+           border-radius: 50%;
+       }
+   }
 </style>

72f4aabe878109f0f81df00c585e63003c55f54a
gaoly <shuiruohanyu@foxmail.com>
2019-12-28 16:16:21 +0800

    黑马头条PC-页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,9 +1,11 @@
 
 // 处理路由拦截器 导航守卫
 import router from '../router'
-
+import progresss from 'nprogress'
+import 'nprogress/nprogress.css'
 // 全局前置守卫  当 路由发生变化时 这个方法里的回调函数就会执行
 router.beforeEach(function (to, from, next) {
+  progresss.start() // 开启进度条
   // 权限拦截 认为有token 让过去 没token不让过
   if (to.path.startsWith('/home')) {
     //   确定要去检查的范围
@@ -16,4 +18,9 @@ router.beforeEach(function (to, from, next) {
   } else {
     next() // 直接放过
   }
+})
+router.afterEach(() => {
+  // setTimeout(() => progresss.done(), 1000)
+  progresss.done()
+  // 关闭进度条
 })

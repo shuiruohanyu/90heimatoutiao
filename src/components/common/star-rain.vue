bc90c3c6409ff30f92fceca7104abeca02a34842
shuiruohanyu <shuiruohanyu@foxmail.com>
2020-03-11 00:40:58 +0800

    提交代码

--- src/components/common/star-rain.vue
+++ src/components/common/star-rain.vue
@@ -1,18 +1,44 @@
 <template>
-  <el-card style='width:600px; height:500px'>
+  <el-card style='width:700px; height:500px;position:relative'>
       <el-row type='flex' justify="center">
           <el-button v-if="!beginRain" @click="beginRain = true" type="primary">开始流星雨</el-button>
           <el-button v-else type="info" @click="beginRain = false">停止</el-button>
       </el-row>
-      <canvas ref="myCanvas" width="500" height="400" style="border: 2px dashed #ccc"></canvas>
+
+     <el-slider
+     style='position:absolute;right:50px'
+      v-model="maxStar"
+      :min="1"
+      :max="10"
+      vertical
+      label="数量"
+      height="200px">
+
+    </el-slider>
+     <el-slider
+     style='position:absolute;right:0'
+      v-model="speed"
+      :min="1"
+      :max="10"
+      vertical
+      label="速率"
+      height="200px">
+
+    </el-slider>
+         <canvas ref="myCanvas" width="500" height="400" style="border: 2px dashed #ccc"></canvas>
   </el-card>
 </template>
 
 <script>
+import Star from '../star'
 export default {
   data () {
     return {
-      beginRain: false
+      beginRain: false,
+      direction: 'left',
+      stars: [],
+      maxStar: 1,
+      speed: 1
     }
   },
   mounted () {
@@ -21,14 +47,53 @@ export default {
   watch: {
     beginRain () {
       this.beginRain ? this.start() : this.stop()
+    },
+    maxStar () {
+      this.stop()
+      this.start()
+    },
+    speed () {
+      this.stop()
+      this.start()
     }
   },
   methods: {
+    next () {
+      this.stars[0].step && this.stars[0].step()
+    },
     start () {
     //   流星雨 就是一根根斜着的线 从画布飘过
     //   要随机生成若干根线 可以有前后事件的顺序 可根据单位时间的产出
+      this.Canvas.fillStyle = '#090723'
+      this.Canvas.fillRect(0, 0, 500, 400)
+      this.starFlag = setInterval(() => this.createStar(), 1000 / this.maxStar)
     },
-    stop () {}
+    // 创建星星
+    createStar () {
+      let obj = new Star({
+        maxWidth: 500,
+        maxHeight: 400,
+        canvas: this.Canvas,
+        direction: this.direction,
+        callBack: this.destoryStar,
+        index: this.stars.length,
+        speed: this.speed
+      })
+      this.stars.push(obj)
+    },
+    stop () {
+      clearInterval(this.starFlag)
+      this.stars.map(item => {
+        item.stopFlay()
+        item = null
+      })
+      this.Canvas.fillStyle = '#fff'
+      this.Canvas.clearRect(0, 0, 500, 400)
+    },
+    destoryStar (index) {
+      this.stars[index] = null // 设置为空 释放空间
+      this.stars.splice(index, 1) // 删除对应的对象
+    }
   }
 }
 </script>
--- src/components/star.js
+++ src/components/star.js
@@ -1,17 +1,21 @@
 class star {
   pointX = 0 // x坐标
-  pointY = 0 // y坐标
+  pointY = 10 // y坐标
   length = 0 // 长度
   canvas = null
   direction = '' // 方向 left / right
   flying = false // 飞行状态
-  constructor ({ pointX, pointY, length, canvas, direction }) {
+  maxWidth = 0
+  maxHeight = 0
+  constructor ({ maxWidth, maxHeight, canvas, direction, callBack, index, speed }) {
     //   props传入起始坐标点 长度 canvasContext对象
-    this.pointX = pointX
-    this.pointY = pointY
-    this.length = length
+    this.maxWidth = maxWidth
+    this.maxHeight = maxHeight
     this.canvas = canvas
     this.direction = direction
+    this.callBack = callBack
+    this.index = index
+    this.speed = speed
     this.startFlay() // 初始化完毕后开始飞翔
   }
   /***
@@ -19,23 +23,89 @@ class star {
    * ****/
   startFlay () {
     if (!this.flying) {
+      this.flying = true
+      this.angel = 45 // 随机的角度
+      this.length = Math.floor(Math.random() * 70) + 40 // 随机的长度
+      this.pointX = Math.floor(Math.random() * this.maxWidth) + 1
 
+      let { width, height } = this.getXYLength(this.length)
+      this.width = width
+      this.height = height
+      // 随机的开始坐标点
+      window.requestAnimationFrame(this.animateFrame.bind(this))
     }
   }
   //   动画
   animateFrame () {
+    // this.canvas.save()
     // 计算坐标
     // 向左时 流星坐标的x起始坐标 大于 终点坐标
     // 向右时 x坐标大于终点坐标
     // 根据勾股定理 推理 起始x y 坐标 (长度) => 终点x y坐标
     // 最终得到 x y 坐标的唯一解 做两侧坐标绝对值
     //   this.direction === 'left' ?
+    // this.clearPath()
+
+    // this.canvas.save()
+    let next = this.getXYLength(this.speed)
+    let newPoint = this.getEndPoint(next.width, next.height)
+    this.pointX = newPoint.endX
+    this.pointY = newPoint.endY
+
+    let endPoint = this.getEndPoint(this.width, this.height)
+    this.canvas.beginPath()
+    // 分段设置颜
+    var line = this.canvas.createLinearGradient(this.pointX, this.pointY,
+      endPoint.endX,
+      endPoint.endY)
+    // 分段设置颜色
+    line.addColorStop(1, '#fff')
+    line.addColorStop(0, '#090723')
+    this.canvas.lineWidth = 1
+    this.canvas.strokeStyle = line
+
+    this.canvas.moveTo(this.pointX, this.pointY)
+    this.canvas.lineTo(endPoint.endX, endPoint.endY)
+    this.canvas.closePath()
+    this.canvas.stroke()
+    // this.canvas.restore()
+
+    if (this.pointX > 0 && this.pointX < this.maxWidth && this.pointY < this.maxHeight && this.flying) {
+      window.requestAnimationFrame(this.animateFrame.bind(this))
+    } else {
+      this.stopFlay()
+      // this.clearPath()
+
+      this.callBack && this.callBack(this.index)
+    }
+  }
+  step () {
+    this.animateFrame()
+  }
+  clearPath () {
+    this.canvas.clearRect(this.pointX - this.width - 3, this.pointY - 3, this.width + 5, this.height + 5)
+  }
+  // 计算结束的坐标点
+  getEndPoint (width, height) {
+    //  数学公式
+    let endY = this.pointY + height
+    let endX = this.pointX + (this.direction === 'left' ? -(width) : width)
+    return {
+      endX, endY
+    }
+  }
+  getXYLength (length) {
+    let width = length * Math.sin(this.angel / 180 * Math.PI) // 算出直角边上 也就是x竖直高度
+    let height = length * Math.cos(this.angel / 180 * Math.PI)
+    return {
+      width, height
+    }
   }
   /****
    *停止飞
    * *****/
   stopFlay () {
-
+    this.flying = false // 停止飞行
   }
 }
 export default star
